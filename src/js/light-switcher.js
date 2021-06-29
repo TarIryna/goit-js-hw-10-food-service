b100cef372b14f56a2048530146fafc0c1cfe3d9
Tar Iryna <tar86irina@gmail.com>
2021-06-30 00:10:50 +0300

    29 06

--- src/js/light-switcher.js
+++ src/js/light-switcher.js
@@ -7,17 +7,18 @@ lightSwitch.addEventListener('change', themeSwitch);
 function themeSwitch() {
     if (lightSwitch.checked) {
         console.log('выбрана темная тема');
-        bodyEl.classList.toggle("dark-theme");
-        // localStorage.setItem('theme', "dark-theme");
+        bodyEl.classList.remove("light-theme");
+        bodyEl.classList.add("dark-theme");
+        localStorage.setItem('theme', "dark-theme");
     }
     else {
         console.log('выбрана светлая тема');
-        // bodyEl.classList.toggle("light-theme");
-        // localStorage.setItem('theme', "light-theme");
+        bodyEl.classList.remove("dark-theme");
+        bodyEl.classList.add("light-theme");
+        localStorage.setItem('theme', "light-theme");
     }
 }
 
 // localStorage.setItem('theme', "${lightSwitch.checked}")
 
 console.log(lightSwitch.checked);
-console.log(localstorage.getItem('theme'));
